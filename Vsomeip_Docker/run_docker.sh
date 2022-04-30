faa830c68af6b2f9076091d66d0cd0dce504c190
Seohyun Han <seohyunhan@Seohyuns-MacBook-Pro.local>
2022-04-30 19:11:29 +0900

    [Docker] Modified docker script for sample env

--- Vsomeip_Docker/run_docker.sh
+++ Vsomeip_Docker/run_docker.sh
@@ -1,1 +1,12 @@
+echo "###Docker Run Script Start###"
+echo "=> Copy all Libraries and Sources to Docker Volume"
+scp -r ./../lib ./install/
+mkdir ./install/sample
+scp ./../CMakeLists.txt ./install/
+scp -r ./../src-gen ./install
+scp -r ./../config ./install/
+scp -r ./../src ./install
+echo "=> Build docker image [Only very first time]"
+docker build . -t vsomeip_test_img
+echo "=> Run Docker"
 docker run -it --name vsomeip_test -v $(pwd)/install:/Workspace/install -w /Workspace/install vsomeip_test_img 
---
+++ Vsomeip_Docker/start_docker.sh
@@ -0,0 +1,5 @@
+echo "###Dcoker Start Script Start###"
+echo "=> Docker container 'vsomeip_test' start"
+docker start vsomeip_test
+echo "=> Attach to Docker container 'vsomeip_test'"
+docker attach vsomeip_test
